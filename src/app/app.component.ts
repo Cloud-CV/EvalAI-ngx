c408830533708583331c15a5e6077b5fbdae7c3f
anweshknayak <anweshknayak@gmail.com>
2018-03-01 11:11:23 +0530

    header component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/mergeMap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'] 
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent implements OnInit {
