4704583190d185085dbf109b69619619852d3142
anweshknayak <anweshknayak@gmail.com>
2018-03-01 21:24:23 +0530

    public declaration

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  private logo = require('./assets/evalai-logo-single.png');
+  public logo = require('./assets/evalai-logo-single.png');
   constructor() { }
 
   ngOnInit() {
