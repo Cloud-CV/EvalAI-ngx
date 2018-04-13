c6b52237310bbed0ec2eaad9b9651ff40a583f9f
Deshraj Yadav <deshraj@gatech.edu>
2018-04-13 15:23:39 -0400

    Update pr_deploy.sh

--- pr_deploy.sh
+++ pr_deploy.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo "Not a PR. Skipping surge deployment"
+    echo "Not a Pull Request. Skipping surge deployment"
     exit 0
 fi
 
 npm i -g surge
 
-export SURGE_LOGIN=test@example.co.in
-# Token of a dummy account.
-export SURGE_TOKEN=d1c28a7a75967cc2b4c852cca0d12206
+export SURGE_LOGIN=$SURGE_LOGIN
+export SURGE_TOKEN=$SURGE_TOKEN
 
-export DEPLOY_DOMAIN=https://pr-${TRAVIS_PULL_REQUEST}-cloudcv-evalai-ngx.surge.sh
+export DEPLOY_DOMAIN=https://pr-${TRAVIS_PULL_REQUEST}-evalai.surge.sh
 surge --project ./dist --domain $DEPLOY_DOMAIN;
