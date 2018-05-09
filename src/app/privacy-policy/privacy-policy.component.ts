1b6524e7fa685a6d85878cdb0d9b5a203337fdb5
Dhruv Batheja <dhruv.life@hotmail.com>
2018-05-09 19:39:29 +0200

    Privacy policy updated after review

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -11,20 +11,20 @@ export class PrivacyPolicyComponent implements OnInit {
   constructor(@Inject(DOCUMENT) private document: Document) { }
 
   isScrolledIntoView(el) {
-    const rect = el.getBoundingClientRect();
-    const elemTop = rect.top;
-    const elemBottom = rect.bottom;
-    const headerHeight = this.document.getElementById('header-static').clientHeight;
+    const RECT = el.getBoundingClientRect();
+    const ELEM_TOP = RECT.top;
+    const ELEM_BOTTOM = RECT.bottom;
+    const HEADER_HEIGHT = this.document.getElementById('header-static').clientHeight;
     // Only completely visible elements return true:
-    const isVisible = (elemTop >= headerHeight) && (elemBottom <= window.innerHeight);
+    const IS_VISIBLE = (ELEM_TOP >= HEADER_HEIGHT) && (ELEM_BOTTOM <= window.innerHeight);
     // Partially visible elements return true:
-    // isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-    return isVisible;
+    // isVisible = ELEM_TOP < window.innerHeight && ELEM_BOTTOM >= 0;
+    return IS_VISIBLE;
   }
 
   highlightNav(el) {
-    const allNavs = this.document.getElementsByClassName('privacy-nav');
-    [].forEach.call(allNavs, function (item) {
+    const ALL_NAVS = this.document.getElementsByClassName('privacy-nav');
+    [].forEach.call(ALL_NAVS, function (item) {
       item.classList.remove('scroll-selected');
       if (item.id === el) {
         item.className += ' scroll-selected';
@@ -33,17 +33,17 @@ export class PrivacyPolicyComponent implements OnInit {
   }
   ngOnInit() {
     this.document['manuallyScrolling'] = false;
-    const idText = 'privacy-';
+    const ID_TEXT = 'privacy-';
     let i = 1;
     const allNavs = this.document.getElementsByClassName('privacy-nav');
     [].forEach.call(allNavs, function (item) {
-      item.id = idText + i.toString() + '-nav';
+      item.id = ID_TEXT + i.toString() + '-nav';
       i = i + 1;
     });
     i = 1;
-    const allTargets = this.document.getElementsByClassName('privacy-section-title');
-    [].forEach.call(allTargets, function (item) {
-      item.id = idText + i.toString();
+    const ALL_TARGETS = this.document.getElementsByClassName('privacy-section-title');
+    [].forEach.call(ALL_TARGETS, function (item) {
+      item.id = ID_TEXT + i.toString();
       i = i + 1;
     });
   }
@@ -51,15 +51,15 @@ export class PrivacyPolicyComponent implements OnInit {
   @HostListener('window:scroll', [])
     onWindowScroll(): void {
     if (!this.document['manuallyScrolling']) {
-        const allTargets = this.document.getElementsByClassName('privacy-section-title');
-        const self = this;
-        [].some.call(allTargets, function (item) {
-          const elem = self.document.getElementById(item.id);
-          if (self.isScrolledIntoView(elem)) {
-            self.highlightNav(item.id + '-nav');
-            const elemNav = self.document.getElementById(item.id + '-nav');
-            if (!self.isScrolledIntoView(elemNav)) {
-              elemNav.scrollIntoView();
+        const ALL_TARGETS = this.document.getElementsByClassName('privacy-section-title');
+        const SELF = this;
+        [].some.call(ALL_TARGETS, function (item) {
+          const elem = SELF.document.getElementById(item.id);
+          if (SELF.isScrolledIntoView(elem)) {
+            SELF.highlightNav(item.id + '-nav');
+            const ELEM_NAV = SELF.document.getElementById(item.id + '-nav');
+            if (!SELF.isScrolledIntoView(ELEM_NAV)) {
+              ELEM_NAV.scrollIntoView();
             }
             return true;
           }
@@ -73,17 +73,17 @@ export class PrivacyPolicyComponent implements OnInit {
       this.document['manuallyScrolling'] = false;
     }, 2000);
     // getting id of the clicked item
-    const target = event.target || event.srcElement || event.currentTarget;
-    const id = target.attributes.id.value;
-    this.highlightNav(id);
+    const TARGET = event.target || event.srcElement || event.currentTarget;
+    const ID = TARGET.attributes.id.value;
+    this.highlightNav(ID);
 
     // Removing -nav from the id of the clicked element
-    const elementId = id.slice(0, -4);
-    this.document.getElementById(elementId).scrollIntoView();
-    const scrolledY = window.scrollY;
-    if (scrolledY) {
-      const headerHeight = this.document.getElementById('header-static').clientHeight;
-      window.scroll(0, scrolledY - headerHeight);
+    const ELEMENT_ID = ID.slice(0, -4);
+    this.document.getElementById(ELEMENT_ID).scrollIntoView();
+    const SCROLLED_Y = window.scrollY;
+    if (SCROLLED_Y) {
+      const HEADER_HEIGHT = this.document.getElementById('header-static').clientHeight;
+      window.scroll(0, SCROLLED_Y - HEADER_HEIGHT);
     }
   }
   scrollToTop() {
