0f7b468bfdff268490ff314757a3738e18d3e376
Dhruv Batheja <dhruv.life@hotmail.com>
2018-07-18 04:47:38 +0200

    Fixed Travis warnings

--- src/app/components/publiclists/challengelist/challengecard/challengecard.component.spec.ts
+++ src/app/components/publiclists/challengelist/challengecard/challengecard.component.spec.ts
@@ -3,6 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChallengecardComponent } from './challengecard.component';
 import { GlobalService } from '../../../../services/global.service';
 import { ApiService } from '../../../../services/api.service';
+import { AuthService } from '../../../../services/auth.service';
+import { ChallengeService } from '../../../../services/challenge.service';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -14,7 +16,7 @@ describe('ChallengecardComponent', () => {
     TestBed.configureTestingModule({
       imports: [ HttpClientModule, RouterTestingModule ],
       declarations: [ ChallengecardComponent ],
-      providers: [ GlobalService, ApiService ]
+      providers: [ GlobalService, ApiService, AuthService, ChallengeService ]
     })
     .compileComponents();
   }));
