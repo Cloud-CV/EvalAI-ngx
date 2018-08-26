3d2ad1c092e985a04a6f6046593f28b243ac7fa9
Dhruv Batheja <dhruv.life@hotmail.com>
2018-08-26 16:38:51 +0200

    Fixed lints and tests

--- src/app/components/our-team/our-team.component.spec.ts
+++ src/app/components/our-team/our-team.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OurTeamComponent } from './our-team.component';
+import { HeaderStaticComponent } from '../../components/nav/header-static/header-static.component';
+import { FooterComponent } from '../nav/footer/footer.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { GlobalService } from '../../services/global.service';
+import { AuthService } from '../../services/auth.service';
+import { EndpointsService } from '../../services/endpoints.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('OurTeamComponent', () => {
   let component: OurTeamComponent;
@@ -8,7 +17,10 @@ describe('OurTeamComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OurTeamComponent ]
+      declarations: [ OurTeamComponent, FooterComponent, HeaderStaticComponent ],
+      imports: [ HttpClientModule, RouterTestingModule ],
+      providers: [ GlobalService, AuthService, EndpointsService, ApiService],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
