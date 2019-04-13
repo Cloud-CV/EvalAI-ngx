2d3c01e2188257ebc5c920abd48c3abcfe1a9cdd
Unknown <sanketbansal57@gmail.com>
2019-04-13 23:58:48 +0530

    Fixed testing issue.
    Added Server-Error component and Not-Found component in the html-errors component spec.ts file.

--- src/app/components/html-errors/html-errors.component.spec.ts
+++ src/app/components/html-errors/html-errors.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HtmlErrorsComponent } from './html-errors.component';
+import {ServerErrorComponent} from './server-error/server-error.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 describe('HtmlErrorsComponent', () => {
   let component: HtmlErrorsComponent;
@@ -8,7 +11,8 @@ describe('HtmlErrorsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HtmlErrorsComponent ]
+      declarations: [ HtmlErrorsComponent, ServerErrorComponent, NotFoundComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
