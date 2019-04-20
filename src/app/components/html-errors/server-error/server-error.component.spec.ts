3ab9df7e6ba72707f80969b26b42f30849dc5ec9
sanket <sanketbansal57@gmail.com>
2019-04-20 18:33:50 +0530

    Added Integration Testing
    Integration testing added for server-error component.
    It checks the value of  <a> to be " EvalAi".
    Also, <a> attribute 'href' value should be ''.

--- src/app/components/html-errors/server-error/server-error.component.spec.ts
+++ src/app/components/html-errors/server-error/server-error.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ServerErrorComponent } from './server-error.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('ServerErrorComponent', () => {
   let component: ServerErrorComponent;
   let fixture: ComponentFixture<ServerErrorComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +19,22 @@ describe('ServerErrorComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ServerErrorComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be EvalAi', () => {
+    expect(component).toBeTruthy();
+    expect(de.query(By.css('p')).query(By.css('a')).nativeElement.innerText).toBe(' EvalAi');
+  });
+
+  it('should be redirected to "" ', () => {
+    expect(component).toBeTruthy();
+    expect(de.query(By.css('p')).query(By.css('a')).attributes.valueOf()['href']).toBe('');
+  });
+
 });
