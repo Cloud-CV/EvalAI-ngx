5df759dd238caeb7d0acaf322e23f6973a65a00d
sanket <sanketbansal57@gmail.com>
2019-05-18 22:06:01 +0530

    Fixed testing error
    Added RouterTestingModule

--- src/app/services/error-handler.service.spec.ts
+++ src/app/services/error-handler.service.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { ErrorHandlerService } from './error-handler.service';
 
 describe('ErrorHandlerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       providers: [ErrorHandlerService]
     });
   });
