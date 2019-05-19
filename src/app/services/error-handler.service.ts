45eaf39992f615a8be217d232f5b8801e13c1413
sanket <sanketbansal57@gmail.com>
2019-05-19 23:25:33 +0530

    changed routing
    changed routing to 'error/500'

--- src/app/services/error-handler.service.ts
+++ src/app/services/error-handler.service.ts
@@ -15,7 +15,7 @@ export class ErrorHandlerService {
   }
 
   private handle500Error(error: HttpErrorResponse) {
-    this.router.navigate(['/500']);
+    this.router.navigate(['/error/500']);
     // TODO: We can extract the error message or text and send to 500 page
     // using this line : `error.error ? error.error : error.statusText`
   }
