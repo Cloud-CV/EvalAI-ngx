dac32b753fd34df0711d8d59478c9f3d5115b39a
sanket <sanketbansal57@gmail.com>
2019-05-22 19:03:05 +0530

    Added global service for interceptor
    GlobalService provided for the interceptor to handleAPIError.

--- src/app/interceptors/http-error-interceptor.ts
+++ src/app/interceptors/http-error-interceptor.ts
@@ -10,20 +10,19 @@ import {
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
-import { ErrorHandlerService } from '../services/error-handler.service';
-
+import {GlobalService} from '../services/global.service';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
 
-  constructor(private errorHandler: ErrorHandlerService) {
+  constructor(private errorHandler: GlobalService) {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     return next.handle(request).do((event: HttpEvent<any>) => { }, (err: any) => {
       if (err instanceof HttpErrorResponse) {
-        this.errorHandler.handleError(err);
+        this.errorHandler.handleApiError(err.status, true);
       }
     });
   }
