37fbdeba2f80a8d587c28a07f5499ff164fd34cb
sanket <sanketbansal57@gmail.com>
2019-05-28 07:55:40 -0400

    Test cases for Auth service
    Added test cases for Auth service to increase its code coverage to 84%.

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -5,8 +5,12 @@ import { GlobalService } from './global.service';
 import { EndpointsService } from './endpoints.service';
 import { ApiService } from './api.service';
 import { HttpClientModule } from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 
 describe('AuthService', () => {
+  let authService: AuthService;
+  let globalService: GlobalService;
+  let apiService: ApiService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [AuthService, GlobalService, ApiService, EndpointsService],
@@ -14,7 +18,51 @@ describe('AuthService', () => {
     });
   });
 
+  beforeEach(() => {
+    authService = TestBed.get(AuthService);
+    globalService = TestBed.get(GlobalService);
+    apiService = TestBed.get(ApiService);
+  });
+
   it('should be created', inject([AuthService], (service: AuthService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should login using isLoggedIn', () => {
+    spyOn(globalService, 'getAuthToken').and.returnValue(true);
+    authService.isLoggedIn();
+    expect(globalService.getAuthToken).toHaveBeenCalled();
+  });
+
+  it('should call logOut', () => {
+    authService.logOut();
+    expect(authService.authState.isLoggedIn).toBe(false);
+  });
+
+  it('should return password strength', () => {
+    const strength = authService.passwordStrength('../Cloudcv#2019')[0];
+    const color = authService.passwordStrength('../Cloudcv#2019')[1];
+    expect(strength).toBe('Very Strong');
+    expect(color).toBe('darkgreen');
+  });
+
+  it('should login using isLoggedIn', () => {
+    spyOn(apiService, 'getUrl').and.returnValue(new Observable((observer) => {
+      observer.next({});
+      observer.complete();
+      return {unsubscribe() {}};
+    }));
+    authService.fetchUserDetails();
+    expect(apiService.getUrl).toHaveBeenCalled();
+  });
+
+  it('should call verifyEmail', () => {
+    spyOn(apiService, 'postUrl').and.returnValue(new Observable((observer) => {
+      observer.next({});
+      observer.complete();
+      return {unsubscribe() {}};
+    }));
+    authService.verifyEmail('cloudCv@testing.com');
+    expect(apiService.postUrl).toHaveBeenCalled();
+  });
 });
