67dc75c8a090e50f7b722fb4d0a0ebd7b8f82a6c
sanket <sanketbansal57@gmail.com>
2019-05-28 07:56:57 -0400

    Test Cases for Challenge service
    Added test cases for Challenge service to increase code coverage to 65%

--- src/app/services/challenge.service.spec.ts
+++ src/app/services/challenge.service.spec.ts
@@ -1,12 +1,22 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject, fakeAsync, tick} from '@angular/core/testing';
 import { ApiService } from './api.service';
 import { ChallengeService } from './challenge.service';
 import { EndpointsService } from './endpoints.service';
 import { HttpClientModule } from '@angular/common/http';
 import { GlobalService } from './global.service';
 import { AuthService } from './auth.service';
+import {WindowService} from './window.service';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+
+
 
 describe('ChallengeService', () => {
+  let authService: AuthService;
+  let apiService: ApiService;
+  let challengeService: ChallengeService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ChallengeService, ApiService, GlobalService, AuthService, EndpointsService],
@@ -14,7 +24,29 @@ describe('ChallengeService', () => {
     });
   });
 
+  beforeEach(() => {
+    authService = TestBed.get(AuthService);
+    apiService = TestBed.get(ApiService);
+    challengeService = TestBed.get(ChallengeService);
+  });
+
   it('should be created', inject([ChallengeService], (service: ChallengeService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should call fetchChallenges', () => {
+    spyOn(apiService, 'getUrl').and.returnValue(new Observable((observer) => {
+      observer.next({'id': 1});
+      observer.complete();
+      return {unsubscribe() {}};
+    }));
+
+    authService.change = new Observable((observer) => {
+      observer.next({'isLoggedIn': true});
+      observer.complete();
+      return {unsubscribe() {}};
+    });
+    challengeService.fetchChallenge(1);
+  });
+
 });
