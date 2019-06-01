1e936faf2124d8ac127e3fe6cbd0ae0ff2a35c1c
sanket <sanketbansal57@gmail.com>
2019-06-01 08:02:41 -0400

    Ported Home Page
    AngularJS home page ported to Angular 7 by converting main controller logic into Home Page class logic.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ApiService} from '../../services/api.service';
+import {EndpointsService} from '../../services/endpoints.service';
+import {AuthService} from '../../services/auth.service';
+import {GlobalService} from '../../services/global.service';
 
 /**
  * Component Class
@@ -8,17 +12,52 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  /**
-   * Constructor.
-   */
-  constructor() {}
+  public user = {};
+  public challengeList = [];
+  userObserve: any;
+  challengeObserve: any;
+  constructor(private apiService: ApiService, private endpointService: EndpointsService, private authService: AuthService,
+              private globalService: GlobalService) { }
 
-  title = 'EvalAI|Home';
+  ngOnInit() {
+    this.init();
+    this.getChallenge();
+  }
 
-  /**
-   * Component on initialized.
-   */
-  ngOnInit() {}
+  init() {
+    this.authService.fetchUserDetails();
+    this.userObserve = this.apiService.getUrl(this.endpointService.userDetailsURL());
+    this.userObserve.subscribe(
+      response => {
+        const status = response.status;
+        const details = response.data;
+        if (status === 200) {
+          this.user['name'] = details.username;
+        }
+      },
+      err => { this.globalService.handleApiError(err); },
+      () => {}
+    );
+  }
+
+  getChallenge() {
+    this.challengeObserve = this.apiService.getUrl(this.endpointService.featuredChallengesURL());
+    this.challengeObserve.subscribe(
+      response => {
+        this.challengeList = response.data;
+      },
+      err => { this.globalService.handleApiError(err); },
+      () => {}
+    );
+
+  }
+
+  hostChallenge() {}
+
+  ngOnDestroy(): void {
+    this.userObserve.unSubscribe();
+    this.challengeObserve.unSubscribe();
+  }
 }
