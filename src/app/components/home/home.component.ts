e013e881786aa1524d272504153b89708d9d9a7f
sanket <sanketbansal57@gmail.com>
2019-06-01 08:02:41 -0400

    Ported Home Page
    AngularJS home page ported to Angular 7 by converting main controller logic into Home Page class logic.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,7 +3,6 @@ import {ApiService} from '../../services/api.service';
 import {EndpointsService} from '../../services/endpoints.service';
 import {AuthService} from '../../services/auth.service';
 import {GlobalService} from '../../services/global.service';
-import {BehaviorSubject} from 'rxjs';
 
 /**
  * Component Class
@@ -17,7 +16,8 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   public user = {};
   public challengeList = [];
-  authServiceSubscription: any;
+  userObserve: any;
+  challengeObserve: any;
   constructor(private apiService: ApiService, private endpointService: EndpointsService, private authService: AuthService,
               private globalService: GlobalService) { }
 
@@ -27,18 +27,26 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   init() {
-    const logInStatus = this.authService.isLoggedIn();
-    this.authServiceSubscription = this.authService.change.subscribe((authState) => {
-      if (authState.isLoggedIn) {
-        this.user = authState;
-      }
-    });
+    this.authService.fetchUserDetails();
+    this.userObserve = this.apiService.getUrl(this.endpointService.userDetailsURL());
+    this.userObserve.subscribe(
+      response => {
+        const status = response.status;
+        const details = response.data;
+        if (status === 200) {
+          this.user['name'] = details.username;
+        }
+      },
+      err => { this.globalService.handleApiError(err); },
+      () => {}
+    );
   }
 
   getChallenge() {
-    this.apiService.getUrl(this.endpointService.featuredChallengesURL()).subscribe(
+    this.challengeObserve = this.apiService.getUrl(this.endpointService.featuredChallengesURL());
+    this.challengeObserve.subscribe(
       response => {
-        this.challengeList = response.results;
+        this.challengeList = response.data;
       },
       err => { this.globalService.handleApiError(err); },
       () => {}
@@ -49,8 +57,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   hostChallenge() {}
 
   ngOnDestroy(): void {
-    if (this.authServiceSubscription) {
-      this.authServiceSubscription.unsubscribe();
-    }
+    this.userObserve.unSubscribe();
+    this.challengeObserve.unSubscribe();
   }
 }
