3f39a37013ab4d308dfef7ad8faae839254f0b0d
sanket <sanketbansal57@gmail.com>
2019-06-04 11:02:09 -0400

    Changes in home component test file
    Removed test cases to evaluate title and h1 element textContent

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -40,8 +40,6 @@ describe('HomeComponent', () => {
       ],
       providers: [
         GlobalService,
-        {provide: ActivatedRoute, useValue: fakeActivatedRoute},
-        {provide: Router, useClass: class { navigate = jasmine.createSpy('navigate'); }},
         AuthService,
         ApiService,
         EndpointsService,
@@ -62,17 +60,4 @@ describe('HomeComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
-
-  it(`should have as title 'EvalAI|Home'`, async(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    const home = fixture.debugElement.componentInstance;
-    expect(home.title).toEqual('EvalAI|Home');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('EvalAI');
-  }));
 });
