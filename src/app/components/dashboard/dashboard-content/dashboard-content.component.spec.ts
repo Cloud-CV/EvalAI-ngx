02f222ee2efe25abad8fc76b1359bd7a8d82bbe1
sanket <sanketbansal57@gmail.com>
2019-06-04 15:24:09 -0400

    Modified test cases
    Removed fake activated routes to work with routerLink  property in <a> tag

--- src/app/components/dashboard/dashboard-content/dashboard-content.component.spec.ts
+++ src/app/components/dashboard/dashboard-content/dashboard-content.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardContentComponent } from './dashboard-content.component';
+import {ApiService} from '../../../services/api.service';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
+import {GlobalService} from '../../../services/global.service';
+import {AuthService} from '../../../services/auth.service';
+import {EndpointsService} from '../../../services/endpoints.service';
 
 describe('DashboardContentComponent', () => {
   let component: DashboardContentComponent;
@@ -8,7 +14,9 @@ describe('DashboardContentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardContentComponent ]
+      declarations: [ DashboardContentComponent ],
+      providers: [ApiService, GlobalService, AuthService, EndpointsService],
+      imports: [HttpClientModule, RouterTestingModule]
     })
     .compileComponents();
   }));
