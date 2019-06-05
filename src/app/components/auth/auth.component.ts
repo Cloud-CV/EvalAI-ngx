02d4cf21ab623e29b9d8b6758204f4886e50cd64
sanket <sanketbansal57@gmail.com>
2019-06-05 12:28:42 -0400

    Modified auth component
    Changes made in auth component to acquire angularJS application UI.

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { GlobalService } from '../../services/global.service';
+import {AuthService} from '../../services/auth.service';
 
 /**
  * Component Class
@@ -10,20 +11,24 @@ import { GlobalService } from '../../services/global.service';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, AfterViewInit {
 
   /**
    * router local instance
    */
   localRouter: any;
 
+  @ViewChild('authContainer') authContainer: ElementRef;
+
   /**
    * Constructor.
    * @param router  Router Injection.
    * @param route  ActivatedRoute Injection.
    * @param globalService  GlobalService Injection.
+   * @param authService
    */
-  constructor(private router: Router, private route: ActivatedRoute, private globalService: GlobalService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private globalService: GlobalService,
+              private authService: AuthService) { }
 
   /**
    * Component on initialization.
@@ -31,7 +36,10 @@ export class AuthComponent implements OnInit {
   ngOnInit() {
     this.localRouter = this.router;
     this.globalService.scrollToTop();
+    this.authService.resetForm();
   }
+
+  ngAfterViewInit(): void {}
 
   /**
    * Navigate to a certain URL.
