f1f69dc13aa33e9d0b1071a489dd6fc5c88ec20a
sanket <sanketbansal57@gmail.com>
2019-06-10 05:16:21 -0400

    Fixed testing issue
    Added services and httpclientmodule, routertestingmodule, formmodule for the test bed of reset-password component

--- src/app/components/auth/reset-password/reset-password.component.spec.ts
+++ src/app/components/auth/reset-password/reset-password.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResetPasswordComponent } from './reset-password.component';
+import {FormsModule} from '@angular/forms';
+import {AuthService} from '../../../services/auth.service';
+import {GlobalService} from '../../../services/global.service';
+import {EndpointsService} from '../../../services/endpoints.service';
+import {ApiService} from '../../../services/api.service';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('ResetPasswordComponent', () => {
   let component: ResetPasswordComponent;
@@ -8,7 +15,9 @@ describe('ResetPasswordComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResetPasswordComponent ]
+      declarations: [ ResetPasswordComponent ],
+      providers: [AuthService, GlobalService, EndpointsService, ApiService],
+      imports: [HttpClientModule, RouterTestingModule, FormsModule]
     })
     .compileComponents();
   }));
