16f8bd8578d3c6365912dfa442878fa74bf43ec0
Sanji515 <sanjeevsinghdevil97@gmail.com>
2019-06-21 15:05:50 +0530

    added service in the modal spec file

--- src/app/components/utility/modal/modal.component.spec.ts
+++ src/app/components/utility/modal/modal.component.spec.ts
@@ -5,6 +5,10 @@ import { InputComponent } from '../input/input.component';
 import { GlobalService } from '../../../services/global.service';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ChallengeService } from '../../../services/challenge.service';
+import { ApiService } from './api.service';
+import { AuthService } from './auth.service';
+import { EndpointsService } from './endpoints.service';
 
 describe('ModalComponent', () => {
   let component: ModalComponent;
@@ -14,7 +18,7 @@ describe('ModalComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ ModalComponent, InputComponent ],
       imports: [ HttpClientModule ],
-      providers: [ GlobalService ],
+      providers: [ GlobalService, ChallengeService, EndpointsService, AuthService, ApiService ],
       schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
