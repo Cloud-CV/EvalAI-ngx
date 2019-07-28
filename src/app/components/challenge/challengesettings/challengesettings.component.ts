30231cd07382686d8314ed78bdc9be9de4f93a2b
Sanji515 <sanjeevsinghdevil97@gmail.com>
2019-07-28 14:35:40 +0530

    added css for the input

--- src/app/components/challenge/challengesettings/challengesettings.component.ts
+++ src/app/components/challenge/challengesettings/challengesettings.component.ts
@@ -114,10 +114,9 @@ export class ChallengesettingsComponent implements OnInit {
     if (email === '') {
       return true;
     }
-    // const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.
-    // ,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    // return regex.test(String(email).toLowerCase());
-}
+    const regex = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
+    return String(email).search (regex) != -1;
+  }
 
   updateBannedEmailList() {
     const SELF = this;
