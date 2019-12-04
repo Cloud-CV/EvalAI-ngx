d00f16d22cd7e360ba30bb0ab4294b9ec2e45d5d
jayy <35180217+nsjcorps@users.noreply.github.com>
2019-12-04 12:59:46 +0100

    Added endpoint for subscribe

--- src/app/services/endpoints.service.ts
+++ src/app/services/endpoints.service.ts
@@ -76,6 +76,14 @@ export class EndpointsService {
     return `${this.challenges}featured/`;
   }
   /**
+   * Subscribe to Newsletters
+   */
+
+  subscribeURL() {
+    return `web/subscribe/`;
+  }
+
+  /**
    * All Participant teams
    */
   allParticipantTeamsURL() {
