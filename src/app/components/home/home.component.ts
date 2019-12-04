e8e768045eb8ddcd9acb2b50b3e0280aaedac897
jayy <35180217+nsjcorps@users.noreply.github.com>
2019-12-04 12:59:10 +0100

    Added form validation for subscribe and subscribe function

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChildren, QueryList, AfterViewInit} from '@angular/core';
 import {ApiService} from '../../services/api.service';
 import {EndpointsService} from '../../services/endpoints.service';
 import {AuthService} from '../../services/auth.service';
 import {GlobalService} from '../../services/global.service';
 import {BehaviorSubject} from 'rxjs';
 import {Router} from '@angular/router';
+import { InputComponent } from '../../components/utility/input/input.component';
 
 /**
  * Component Class
@@ -14,12 +15,23 @@ import {Router} from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
-
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   challengeCreateRoute = '/challenge-create';
   authRoute = '/auth/login';
   public user = {};
   public challengeList = [];
+
+  /**
+ * Subscribe Form
+ */
+
+  SUBSCRIBE_FORM: any = {};
+  subscribeForm = "formgroup";
+
+  @ViewChildren('formgroup')
+  components: QueryList<InputComponent>;
+
+
   authServiceSubscription: any;
   constructor(private apiService: ApiService, private endpointService: EndpointsService, private authService: AuthService,
               private globalService: GlobalService, private router: Router) { }
@@ -27,6 +39,13 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.init();
     this.getChallenge();
+  }
+
+  /**
+   * Set SUBSCRIBE_FORM to this.components after view initialization
+   */
+  ngAfterViewInit() {
+    this.SUBSCRIBE_FORM[this.subscribeForm] = this.components;
   }
 
   init() {
@@ -56,9 +75,38 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+   * Form submit function (Called after validation).
+   * @param self  context value of this
+   * Validate Subscribe Form
+   */
+  formValidate(formname) {
+    this.globalService.formValidate(this.SUBSCRIBE_FORM[this.subscribeForm], this.subscribe, this);
+  }
+
+  /**
+   * Subscribe function - POST email to Subscribe URL for evaluation
+   */
+  subscribe(self) {
+    const SUBSCRIBE_BODY = JSON.stringify({
+      email: self.globalService.formValueForLabel(self.SUBSCRIBE_FORM[self.subscribeForm], 'email'),
+    });
+    self.apiService.postUrl(self.endpointService.subscribeURL(), SUBSCRIBE_BODY).subscribe(
+      response => {
+        // Success Message in response.message
+        setTimeout(() => self.globalService.showToast('success', response.message, 5), 1000);
+      },
+      // Show Form Error on Failure
+      err => { self.globalService.handleApiError(err); console.log(err)},
+      () => {}
+    );
+  }
+
   ngOnDestroy(): void {
     if (this.authServiceSubscription) {
       this.authServiceSubscription.unsubscribe();
     }
   }
+
+
 }
