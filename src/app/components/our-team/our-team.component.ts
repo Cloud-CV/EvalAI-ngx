8c6d8d3ed8849c7b9b649a5723c212671af54998
n-bernat <n.bernat@wp.pl>
2019-12-12 21:56:58 +0100

    Update 2.1 - Fixed failing checks

--- src/app/components/our-team/our-team.component.ts
+++ src/app/components/our-team/our-team.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { GlobalService } from '../../services/global.service';
 import { EndpointsService } from '../../services/endpoints.service';
@@ -13,7 +13,7 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: './our-team.component.html',
   styleUrls: ['./our-team.component.scss']
 })
-export class OurTeamComponent implements OnInit {
+export class OurTeamComponent implements OnInit, AfterViewChecked {
 
    /**
    * Constructor.
@@ -75,17 +75,16 @@ export class OurTeamComponent implements OnInit {
 
   ngAfterViewChecked() {
      const DIVS = document.querySelectorAll('.team-player');
-     
+
      for (let i = 0; i < DIVS.length; i++) {
-        let tooltip = DIVS[i].getElementsByClassName('text-name-tooltip')[0];
-        let name = DIVS[i].getElementsByClassName('text-dark-black')[0];
-        
+        const TOOLTIP = DIVS[i].getElementsByClassName('text-name-tooltip')[0];
+        const NAME = DIVS[i].getElementsByClassName('text-dark-black')[0];
+
         // Check if name is collapsed
-        if (name.scrollHeight > name.clientHeight) {
-           tooltip.className = "text-name-tooltip enabled";
-        }
-        else {
-           tooltip.className = "text-name-tooltip disabled";
+        if (NAME.scrollHeight > NAME.clientHeight) {
+           TOOLTIP.className = 'text-name-tooltip enabled';
+        } else {
+           TOOLTIP.className = 'text-name-tooltip disabled';
         }
      }
   }
