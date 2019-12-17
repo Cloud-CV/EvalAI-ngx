6403cb0efc4408b6aa56df0fcbfb3435d61d8b29
Ryan Lee <ryanlee7132@gmail.com>
2019-12-17 07:46:03 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -71,12 +71,12 @@ export class AppComponent implements OnInit, OnDestroy {
  constructor(private router: Router) {
     router.events.forEach((event) => {
       if (event instanceof NavigationStart) {
-        if (event['url'] == '/components/auth' || event['url'] == '/components/dashboard') {
+        if (event['url'] == '/components/auth' || event['url'] == '/login'{
           this.header = false;
         } else {
           this.header = true;
         }
-        if (event['url'] == '/components/auth') {
+        if (event['url'] == '/components/auth' || event['url'] == '/login'{
           this.footer = false;
         } else {
           this.footer = true;
