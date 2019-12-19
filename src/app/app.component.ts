75520948fdcfa9834f077f1defcf5bc586399a4e
Ryan Lee <ryanlee7132@gmail.com>
2019-12-18 22:47:57 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,7 +46,7 @@ export class AppComponent implements OnInit, OnDestroy {
   public activatedRoute: ActivatedRoute,
   public titleService: Title,
   private globalService: GlobalService,
-  private authService: AuthService
+  public authService: AuthService
   ) {
   }
 
