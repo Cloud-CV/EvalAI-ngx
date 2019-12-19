52cbcf47600ae74b85d3a0cb3fa443677b01cb5f
Ryan Lee <ryanlee7132@gmail.com>
2019-12-18 23:46:55 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,15 +50,15 @@ export class AppComponent implements OnInit, OnDestroy {
   ) {
     router.events.forEach((event) => {
       if (event instanceof NavigationStart) {
-        if (event['url'] == '/auth' || event['url'] == '/dashboard' || event['url'] == '/analytics' || event['url'] == '/challenge-create') {
+        if (event['url'] === '/auth' || event['url'] === '/dashboard' || event['url'] === '/analytics' 
+            || event['url'] === '/challenge-create') {
           this.showNav = false;
         } else {
           this.showNav = true;
         }
       }
     });
-  }    
-  
+  }
   /**
    * Scroll event listener.
    */
