09a1166566c0b91337a81415183d63240897189a
Andre Christoga Pramaditya <caxvis@gmail.com>
2019-12-22 00:13:14 +0700

    Changes necesarry

--- src/app/components/utility/modal/modal.component.ts
+++ src/app/components/utility/modal/modal.component.ts
@@ -239,12 +239,8 @@ export class ModalComponent implements OnInit {
     let finalText = '';
 
     if (fields.length) {
-      if (fields.length >= 1) {
-        if (fields.length > 2) {
-          finalText = `${fields.join(', ')} are not valid. \n\n\nBe sure to include HTTP/HTTPS.`;
-        } else {
-          finalText = `${fields.join(' and ')} are not valid. \n\n\nBe sure to include HTTP/HTTPS.`;
-        }
+      if (fields.length > 1) {
+        finalText = `${fields.join(', ')} are not valid. \n\n\nBe sure to include HTTP/HTTPS.`;
       } else {
         finalText = `${fields.join('')} is not valid. \n\n\nBe sure to include HTTP/HTTPS.`;
       }
@@ -279,12 +275,9 @@ export class ModalComponent implements OnInit {
         this.invalidFields = this.invalidFields.filter(element => element !== 'Google Scholar');
         this.convertFieldArrayIntoText(this.invalidFields);
       } else {
-
         if (!(this.invalidFields.find(element => element === 'Google Scholar'))) {
           this.invalidFields.push('Google Scholar');
         }
-        this.convertFieldArrayIntoText(this.invalidFields);
-        this.isDisabled = true;
       }
     } else if (e.target.name === 'update_github_url') {
       if (this.validURL(e.target.value) || e.target.value === '') {
@@ -293,12 +286,9 @@ export class ModalComponent implements OnInit {
         this.invalidFields = this.invalidFields.filter(element => element !== 'GitHub');
         this.convertFieldArrayIntoText(this.invalidFields);
       } else {
-
         if (!(this.invalidFields.find(element => element === 'GitHub'))) {
           this.invalidFields.push('GitHub');
         }
-        this.convertFieldArrayIntoText(this.invalidFields);
-        this.isDisabled = true;
       }
     } else if (e.target.name === 'update_linkedin_url') {
       if (this.validURL(e.target.value) || e.target.value === '') {
@@ -307,12 +297,9 @@ export class ModalComponent implements OnInit {
         this.invalidFields = this.invalidFields.filter(element => element !== 'LinkedIn');
         this.convertFieldArrayIntoText(this.invalidFields);
       } else {
-
         if (!(this.invalidFields.find(element => element === 'LinkedIn'))) {
           this.invalidFields.push('LinkedIn');
         }
-        this.convertFieldArrayIntoText(this.invalidFields);
-        this.isDisabled = true;
       }
     } else if (e.target.name === 'old_password') {
       this.oldPassword = e.target.value;
@@ -326,6 +313,11 @@ export class ModalComponent implements OnInit {
       if (e.target.value !== this.newPassword) {
         this.inputErrorMessage = 'Password do not match';
       }
+    }
+
+    if (this.invalidFields.length >= 1) {
+      this.convertFieldArrayIntoText(this.invalidFields);
+      this.isDisabled = true;
     }
   }
 
