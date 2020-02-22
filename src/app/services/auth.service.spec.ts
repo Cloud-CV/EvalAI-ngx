5baf0dd07ae7b4a249ff4af49f4a2f34ee9c8aec
kajol <kajolkumarisingh222@gmail.com>
2020-02-22 12:46:24 +0530

    added_some_more_tests

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -8,14 +8,47 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthService', () => {
+  let authService: AuthService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [AuthService, GlobalService, ApiService, EndpointsService],
       imports: [ RouterTestingModule, HttpClientModule ]
     });
+    authService = TestBed.get(AuthService);
   });
 
   it('should be created', inject([AuthService], (service: AuthService) => {
     expect(service).toBeTruthy();
   }));
+  it('should change auth state', () => {
+    authService.authStateChange('true');
+  });
+  it('should log In auth state', () => {
+    authService.loggedIn();
+  });
+  it('should log out', () => {
+    authService.logOut();
+  });
+  it('should fetch user details', () => {
+    authService.fetchUserDetails();
+  });
+  it('should give password strength', () => {
+    expect(authService.passwordStrength('passwordgoeshere!')).toEqual(['Average', 'darkorange']);
+    expect(authService.passwordStrength('!very34_#strong$password!')).toEqual(['Strong', 'darkgreen']);
+  });
+  it('should show whether user is logged In', () => {
+    expect(authService.isLoggedIn()).toBe(undefined);
+  });
+  it('should verify email details', () => {
+    authService.verifyEmail('token goes here!');
+  });
+  it('should toggle password', () => {
+    authService.togglePasswordVisibility();
+  });
+  it('should toggle password visibility', () => {
+    authService.toggleConfirmPasswordVisibility();
+  });
+  it('should reset form', () => {
+    authService.resetForm();
+  });
 });
--- src/app/services/challenge.service.spec.ts
+++ src/app/services/challenge.service.spec.ts
@@ -7,15 +7,38 @@ import {RouterTestingModule} from '@angular/router/testing';
 import { GlobalService } from './global.service';
 import { AuthService } from './auth.service';
 
-describe('ChallengeService', () => {
+fdescribe('ChallengeService', () => {
+  let challengeService: ChallengeService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ChallengeService, ApiService, GlobalService, AuthService, EndpointsService],
       imports: [ RouterTestingModule, HttpClientModule ]
     });
+    challengeService = TestBed.get(ChallengeService);
   });
 
   it('should be created', inject([ChallengeService], (service: ChallengeService) => {
     expect(service).toBeTruthy();
   }));
+  it('should update current challenge' , () => {
+    challengeService.changeCurrentChallenge({team_name: 'new_team_name'});
+  });
+  it('should update user host' , () => {
+    challengeService.changeChallengeHostStatus(true);
+  });
+  it('should update challenge publish' , () => {
+    challengeService.changeChallengePublish('new_publish_challenge');
+  });
+  it('should update current stars' , () => {
+    challengeService.changeCurrentStars({stars: 5});
+  });
+  it('should change current participation team' , () => {
+    challengeService.changeCurrentParticipantTeams('new_participation_team');
+  });
+  it('should change current phase' , () => {
+    challengeService.changeCurrentPhases('ongoing');
+  });
+  it('should change current participation status' , () => {
+    challengeService.changeParticipationStatus('ongoing');
+  });
 });
