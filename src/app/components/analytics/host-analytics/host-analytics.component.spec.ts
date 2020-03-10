844e4f774cc7dcf5644386713960bdfac62ac3c0
kajol <kajolkumarisingh222@gmail.com>
2020-03-10 14:45:19 +0530

    host_analytic_test_improved

--- src/app/components/analytics/host-analytics/host-analytics.component.spec.ts
+++ src/app/components/analytics/host-analytics/host-analytics.component.spec.ts
@@ -11,6 +11,7 @@ import {Routes} from '@angular/router';
 import {NotFoundComponent} from '../../not-found/not-found.component';
 import {WindowService} from '../../../services/window.service';
 import {FormsModule} from '@angular/forms';
+import { Observable } from 'rxjs';
 
 
 const routes: Routes = [
@@ -30,7 +31,7 @@ const routes: Routes = [
 ];
 
 
-
+let Observer: (observer) => void;
 describe('HostAnalyticsComponent', () => {
   let component: HostAnalyticsComponent;
   let fixture: ComponentFixture<HostAnalyticsComponent>;
@@ -53,63 +54,94 @@ describe('HostAnalyticsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should check all variable', () => {
-    expect(component.hostTeam).toEqual([]);
-    expect(component.challengeListCount).toBe(0);
-    expect(component.challengeList).toEqual([]);
-    expect(component.isTeamSelected).toBe(false);
-    expect(component.challengeId).toBeNull();
-    expect(component.currentChallengeDetails).toEqual({});
-    expect(component.currentPhase).toEqual([]);
-    expect(component.totalSubmission).toEqual({});
-    expect(component.totalParticipatedTeams).toEqual({});
-    expect(component.lastSubmissionTime).toEqual({});
-    expect(component.totalChallengeTeams).toEqual([]);
-    expect(component.routePath).toEqual('/auth/login');
- });
-  it('should show errCallBack', inject([GlobalService], (service: GlobalService) => {
-    const err1 = {
-       status: '403'
-    };
-    const err2 = {
-      status: '401'
-   };
-    spyOn(service, 'stopLoader').and.callThrough();
-    spyOn(service, 'handleApiError').and.callThrough();
-    spyOn(service, 'showToast').and.callThrough();
-    component.errCallBack(err1);
-    component.errCallBack(err2);
-    expect(service.showToast).toHaveBeenCalled();
-    expect(service.stopLoader).toHaveBeenCalled();
-    expect(service.handleApiError).toHaveBeenCalled();
-  }));
   it('should show HostTeam', inject([ApiService, GlobalService], (service: ApiService, service2: GlobalService) => {
-    spyOn(service, 'getUrl').and.callThrough();
+    spyOn(service, 'getUrl').and.returnValue(new Observable((observer) => {
+      observer.next({'status' : 200});
+      observer.complete();
+      return {unsubscribe() {}};
+    }));
+    fixture.detectChanges();
+    spyOn(service2, 'showToast').and.callThrough();
     component.getHostTeam();
     expect(service.getUrl).toHaveBeenCalled();
+    expect(service2.showToast).not.toHaveBeenCalled();
+  }));
+  it('should give error for HostTeam', inject([ApiService, GlobalService], (service: ApiService, service2: GlobalService) => {
+    const err  = {
+      'status': 403,
+      'error': 'error'
+     };
+     spyOn(service, 'getUrl').and.returnValue(new Observable((observer) => {
+       observer.error(err);
+       return {unsubscribe() {}};
+     }));
+     fixture.detectChanges();
+     spyOn(service2, 'showToast').and.callThrough();
+     component.getHostTeam();
+     err['status'] = 401;
+     component.getHostTeam();
+     expect(service.getUrl).toHaveBeenCalled();
+     expect(service2.showToast).toHaveBeenCalled();
   }));
   it('should show challenge Host',  inject([ApiService, GlobalService], (service: ApiService, service2: GlobalService) => {
-    spyOn(service, 'getUrl').and.callThrough();
+    spyOn(service, 'getUrl').and.returnValue(new Observable((observer) => {
+      observer.next({'results': []});
+      observer.complete();
+      return { unsubscribe() {}};
+    }));
+    fixture.detectChanges();
     component.getChallengeHost();
     expect(service.getUrl).toHaveBeenCalled();
   }));
   it('should show challenge Analysis',  inject([ApiService, GlobalService, EndpointsService],
     (service: ApiService, service2: GlobalService, service3: EndpointsService) => {
-    component.showChallengeAnalysis();
-    expect(component.isTeamSelected).toEqual(false);
-    component.challengeId = 1;
+    const result = {
+      'participant_team_count': 1,
+      'results': [{'id': '1'}],
+      'challenge_phase': '1',
+      'last_submission_timestamp_in_challenge_phase': '2020-03-10',
+      'status': 403,
+      'error': 'err'
+    };
+    // expect(component.isTeamSelected).toEqual(false);
+    // component.challengeId = 1;
+    spyOn(service, 'getUrl').and.returnValue(new Observable((observer) => {
+      observer.next(result);
+      observer.error(result);
+      observer.complete();
+      return {unsubscribe() {}};
+    }));
+    fixture.detectChanges();
     spyOn(service2, 'startLoader').and.callThrough();
     spyOn(service3, 'teamCountAnalyticsURL').and.callThrough();
-    spyOn(service, 'getUrl').and.callThrough();
+    component.showChallengeAnalysis();
+    component.challengeId = '1';
+    component.challengeList = [{'id': 1}];
     component.showChallengeAnalysis();
     expect(service2.startLoader).toHaveBeenCalled();
     expect(service.getUrl).toHaveBeenCalled();
     expect(service3.teamCountAnalyticsURL).toHaveBeenCalled();
   }));
-  it('should show download challenge participant team', inject([EndpointsService], (service: EndpointsService) => {
+  it('should show download challenge participant team', inject([EndpointsService, ApiService],
+    (service: EndpointsService, service2: ApiService) => {
+   const result = {
+     'results' : [{'id': '1'}],
+     'error': {'error': 'Test Error'}
+   };
+   Observer = (observer) => {
+    observer.next(result);
+    observer.error(result);
+    observer.next(result);
+    observer.complete();
+    return {unsubscribe() {}};
+   };
+    spyOn(service2, 'getUrl').and.returnValue(new Observable (Observer));
+    fixture.detectChanges();
     spyOn(service, 'downloadParticipantsAnalyticsURL').and.callThrough();
+    component.challengeId = '1';
     component.downloadChallengeParticipantTeams();
     expect(service.downloadParticipantsAnalyticsURL).toHaveBeenCalled();
+    expect(service2.getUrl).toHaveBeenCalled();
   }));
 
 
