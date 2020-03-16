52b314e27a5085f480d9999cbb0995c92abaf33d
kajol <kajolkumarisingh222@gmail.com>
2020-03-16 15:39:25 +0530

    custom_directive_added

--- src/app/Directives/authcheck.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AuthcheckDirective } from './authcheck.directive';
-
-describe('AuthcheckDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AuthcheckDirective();
-    expect(directive).toBeTruthy();
-  });
-});
