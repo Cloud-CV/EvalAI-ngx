79b569c4d7255028e641f4d40588378c4c5335b7
kajol <kajolkumarisingh222@gmail.com>
2020-03-18 07:06:19 +0530

    test_for_directive_added

---
+++ src/app/Directives/authcheck.directive.spec.ts
@@ -0,0 +1,42 @@
+import { TemplateRef, ViewContainerRef } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+
+// import Directive
+import { AuthcheckDirective } from './authcheck.directive';
+
+// import services
+import { AuthService } from '../services/auth.service';
+import { GlobalService } from '../services/global.service';
+import { ApiService } from '../services/api.service';
+import { EndpointsService } from '../services/endpoints.service';
+
+describe('AuthcheckDirective', () => {
+  let templateRef: jasmine.SpyObj<TemplateRef<any>>;
+  let viewcontainerRef: jasmine.SpyObj<ViewContainerRef>;
+  let authServive: AuthService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AuthcheckDirective
+      ],
+      imports: [
+        RouterTestingModule,
+        HttpClientModule
+      ],
+      providers: [
+        AuthService,
+        GlobalService,
+        ApiService,
+        EndpointsService
+      ]
+    }).compileComponents();
+    authServive = TestBed.get(AuthService);
+  }));
+  it('should create', () => {
+    const authDirective = new AuthcheckDirective(authServive, templateRef, viewcontainerRef);
+    expect(authDirective).toBeTruthy();
+  });
+});
