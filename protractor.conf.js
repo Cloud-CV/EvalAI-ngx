18588ef0054d5d277c28cbc23dcd54ee05fc7a9c
kajol <kajolkumarisingh222@gmail.com>
2020-05-17 00:05:13 +0530

    testing_build

--- protractor.conf.js
+++ protractor.conf.js
@@ -11,6 +11,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome'
   },
+  chromeDriver: '../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_73.0.3683.68',
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
